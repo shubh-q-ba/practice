bfd28442c1098d2929951084bfb43a3bcba659c6
shubh-q-ba <shubham.barsaiyan@quantiphi.com>
2017-07-10 17:20:23 +0530

    morning practice

---
+++ firstlesson.py
@@ -0,0 +1,14 @@
+
+string = raw_input("Enter string:")
+reverse_string = reversed(string)
+if list(string) == list(reverse_string):
+      print("The string is a palindrome")
+else:
+      print("The string isn't a palindrome")
+
+
+
+
+
+
+ECHO is on.
