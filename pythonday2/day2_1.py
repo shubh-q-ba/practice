0fc2f6540bff9ba54816cf4cfb3ed23a09f810eb
shubh-q-ba <shubham.barsaiyan@quantiphi.com>
2017-07-11 16:41:36 +0530

    problems solved today, learned loops

---
+++ pythonday2/day2_1.py
@@ -0,0 +1,5 @@
+num=[]
+for n in range(1500, 2700):
+    if (n%7==0) and (n%5==0):
+        num.append(str(n))
+print num
---
+++ pythonday2/day2_3.py
@@ -0,0 +1,9 @@
+for n in range(0,5):
+    n +=1
+    print ("*" *(0+n))
+for n in range(-5,0):
+    n +=1
+    print ("*" *(0-n+1))
+
+    
+    
